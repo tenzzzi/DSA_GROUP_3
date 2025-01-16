6fcda1da30a0f3eed02b3031ebfa6ccc8735dadd
Lemon, Tines Jimay I. <151240426+kefisiren30@users.noreply.github.com>
2025-01-16 10:24:48 +0800

    Lemon, Tines Jimay I

---
+++ tblproperties.sql
@@ -0,0 +1,17 @@
+/*CREATE database realEstateProperty;*/
+
+/*create table tblProperties(
+	propertyID int,
+    location varchar(50),
+    price int,
+    propertyStatus varchar(30)
+);*/
+
+insert into tblproperties (
+propertyID,
+location,
+price,
+propertyStatus
+)values
+(123456789, "Bacoor, Cavite", 250000, "on going"),
+(234567891, "Calamba, Laguna", 340000, "done")
